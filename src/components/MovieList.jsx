a215cd178eb58233493639c9d1de5920706c8f13
Victoria Hernandez <vlhernan@meta.com>
2025-06-09 17:07:45 -0700

    this commit is for my layout and its not working fully, not centered

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,34 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import MovieCard from "./MovieCard.jsx";
 import "./MovieList.css";
-import { parseData, chunkArray } from "/Users/vlhernan/internship/codepath/flixster-starter/src/utils/helper-functions.js";
+import {parseData} from "/Users/vlhernan/internship/codepath/flixster-starter/src/utils/helper-functions.js";
 
-
-
-function MovieList(props) {
-  // props: data
-  // data: poster_path, title, rating
+function Forecast(props) {
+    //props: data
+    //data: poster_path, title, rating
 
   const parsedData = parseData(props.data);
-  const movieCards = parsedData.map((obj, idx) => (
-    <MovieCard
-      poster_path={obj.poster_path}
-      title={obj.title}
-      rating={obj.rating}
-    />
-  ));
-
-  const rows = chunkArray(movieCards, 6);
 
   return (
-    <div>
-      {rows.map((row, idx) => (
-        <div className="row" key={idx}>
-          {row}
-        </div>
-      ))}
+    <div className="row">
+    {
+        parsedData.map(obj => {
+            return(<MovieCard poster_path={obj.poster_path} title={obj.title} rating={obj.rating} />);
+        })
+        }
     </div>
   );
 }
 
-export default MovieList;
+export default Forecast;
