263c7518775a5b936af53fd147ed5707299aedf5
Victoria Hernandez <vlhernan@meta.com>
2025-06-11 16:23:16 -0700

    added default posters when image is broken, updated card size to 300px to make it more accessible on phone

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,17 +3,26 @@ import ReactDOM from "react-dom";
 import "./MovieCard.css"
 
 function MovieCard(props) {
+  //props: poster_path, title, rating, onCardClick
 
   const handleClick = (event) => {
     props.onCardClick(props.title);
   }
 
-    //props: poster_path, title, rating, onCardClick
+  const getImage = () => {
+    if (props.poster_path === null) {
+      return 'src/assets/placeholder-poster.jpg'; // default poster if there is no image
+    }
+    else{
+      return `https://image.tmdb.org/t/p/w300${props.poster_path}`;
+    }
+  }
+
   return (
     <div className="card" onClick={handleClick}>
-        <img className="card-poster" src={`https://image.tmdb.org/t/p/w300${props.poster_path}`} alt={props.title} />
-        <p className="card-title">{props.title}</p>
-        <p className="card-rating">Rating: {props.rating}</p>
+      <img className="card-poster" src={getImage()} alt={props.title} />
+      <p className="card-title">{props.title}</p>
+      <p className="card-rating">Rating: {props.rating}</p>
     </div>
   );
 }
